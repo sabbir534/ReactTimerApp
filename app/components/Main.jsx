aa4a428477b3ab737dde6ec02ba4ef12af72c573
Sabbir Ahmed <prince534534@gmail.com>
2017-02-16 05:25:47 +0600

    tested countdown form

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -2,15 +2,14 @@ var React = require('react');
 var Navigation = require('Navigation');
 
 var Main = (props) => {
-    return(
-        <div>           
-            <div >
-                <div>
-                    <Navigation/>
-                    <p>Main.jsx</p>
+    return (
+        <div>
+            <Navigation />
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
                     {props.children}
                 </div>
-            </div>  
+            </div>
         </div>
     );
 }
