0940b127bf46eefcd332ecaa9dc7e6086fdbf7ce
Sabbir Ahmed <prince534534@gmail.com>
2017-02-15 20:32:50 +0600

    Create Timer and Countdown Page

--- app/app.jsx
+++ app/app.jsx
@@ -2,13 +2,17 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var { Route, Router, IndexRoute, hashHistory } = require('react-router');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 require('style!css!sass!applicationStyle');
 ReactDOM.render(
     <Router history={hashHistory}>
-        <Route path="/" component={Main}>            
+        <Route path="/" component={Main}>
+            <IndexRoute component={Timer}/>
+            <Route path="/countdown" component={Countdown}/>           
         </Route>
     </Router>,
     document.getElementById('app')
---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+    render(){
+        return(
+            <div>Countdown.jsx</div>
+        )
+    }
+});
+
+module.exports = Countdown;
