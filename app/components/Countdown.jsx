034ad300cd70830009e6867c821e67a8258ac527
Sabbir Ahmed <prince534534@gmail.com>
2017-02-16 00:52:09 +0600

    style and test clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
     render(){
         return(
-            <div>Countdown.jsx</div>
+            <div>
+                <Clock totalSeconds={129}/>
+            </div>
         )
     }
 });
