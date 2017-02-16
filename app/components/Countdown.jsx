04c261fe33de204f1dfe8b6e875d69ca3837d9e2
Sabbir Ahmed <prince534534@gmail.com>
2017-02-17 03:50:20 +0600

    component will unmount

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -13,7 +13,7 @@ var Countdown = React.createClass({
     componentDidUpdate: function(prevProps, prevState) {
         if(this.state.countdownStatus !== prevState.countdownStatus) {
             switch(this.state.countdownStatus) {
-                case 'started':
+                case 'started':                    
                     this.startTimer();
                     break;
                 case 'stopped':
@@ -24,13 +24,21 @@ var Countdown = React.createClass({
                     break;
             }
         }
+    },  
+    componentWillUnmount: function(){
+        clearInterval(this.timer);
+        this.timer = undefined;
     },
+
     startTimer: function(){
         this.timer = setInterval(()=>{
             var newCount = this.state.count - 1;
             this.setState({
                 count: newCount >= 0 ? newCount : 0 
             });
+            if(newCount === 0){
+                this.setState({countdownStatus: 'stopped'});
+            }
         }, 1000);
     },
     handleSetCountdown: function(seconds) {
