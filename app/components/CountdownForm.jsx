b105a4b551156c80e07626419f01bad7cc91333c
Sabbir Ahmed <prince534534@gmail.com>
2017-02-17 06:04:23 +0600

    Finished counter app

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -4,7 +4,8 @@ var CountdownForm = React.createClass({
     onSubmit: function(e){
         e.preventDefault();
         var strSeconds = this.refs.seconds.value;
-        if(strSeconds.match(/^[0-9]*$/)) {
+        
+        if(strSeconds.match(/^[0-9]*$/) && strSeconds !== "") {
             this.refs.seconds.value = '';
             this.props.onSetCountdown(parseInt(strSeconds, 10));
         }
